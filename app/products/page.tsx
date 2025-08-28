9b8a5f4dc48cc8b6a4cc9d7947a9ac34088c692a
Ezequiel Lino <sonisombra@gmail.com>
2025-08-27 18:03:07 -0600

    creacion de contenedores principales

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,6 @@
 
 export default function ProductsPage() {
+  
   return (
     <div>ProductsPage</div>
   )
---
+++ components/order/OrderSummary.tsx
@@ -0,0 +1,8 @@
+
+export default function OrderSummary() {
+  return (
+    <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 py-5">
+        <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
+    </aside>
+  )
+}
