4935fc27d6e0efa458bb065d133ab8effef071bd
Ezequiel Lino <sonisombra@gmail.com>
2025-09-02 15:59:32 -0600

    data fetching en nexts.js y correccion al comando npx prisma db seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
 import { categories } from "./data/categories";
 import { products } from './data/products';
-import { PrismaClient } from './generated/prisma';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
