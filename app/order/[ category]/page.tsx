c00da241fca092ab5f3030147da158cd2263f778
Ezequiel Lino <sonisombra@gmail.com>
2025-09-04 00:05:38 -0600

    detectar la categoria actual y obtener los productos

--- app/order/[ category]/page.tsx
+++ app/order/[ category]/page.tsx
@@ -1,6 +1,20 @@
+import { prisma } from "@/src/lib/prisma";
 
-export default function OrderPage({params}: {params : {category: string}}) {
-  console.log(params.category)
+async function getProducts(category: string){
+  const products = await prisma.product.findMany({
+    where:{
+      category:{
+        slug:category
+      }
+    }
+  })
+
+  return products
+}
+
+export default async function OrderPage({params}: {params : {category: string}}) {
+  const products = await getProducts(params.category)
+
   return (
     <div>OrderPage</div>
   )
