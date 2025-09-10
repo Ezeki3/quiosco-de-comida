5ab59fefb8c6fb082ad43065b7a0f88929b5cb21
Ezequiel Lino <sonisombra@gmail.com>
2025-09-09 23:29:45 -0600

    comunicar el boton con el store

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,16 @@
 import { create } from "zustand";
 import { OrederItem } from "./types";
+import { Product } from "@prisma/client";
 
 interface Store {
     order: OrederItem[]
+    addToOrder: (product: Product) => void
 }
 
 export const useStore = create<Store>(() => ({
-    order: []
+    order: [],
+    addToOrder: (product) => {
+        console.log('agregando', product);
+        
+    }
 }))
