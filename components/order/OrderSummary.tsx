23042695ac6e9c139ee48a17c68a2127385f2b14
Ezequiel Lino <sonisombra@gmail.com>
2025-09-18 23:26:56 -0600

    creando un server action

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -3,17 +3,22 @@ import { useStore } from "@/src/store";
 import ProductDetails from "./ProductDetails";
 import { useMemo } from "react";
 import { formatCurrency } from "@/src/utils";
+import { createOrder } from "@/actions/create-order-action";
 
 export default function OrderSummary() {
 
   const order = useStore((state) => state.order)
   const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
 
+  const handleCreateOrder = () => {
+    createOrder()
+  }
+
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 py-5">
       <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
 
-      {order.length === 0 ? <p className="text-center my-10">El carrito esta vacio </p> : (
+      {order.length === 0 ? <p className="text-center my-10">El pedido esta vacio </p> : (
         <div className="mt-5">
           {order.map(item => (
             <ProductDetails
@@ -26,6 +31,15 @@ export default function OrderSummary() {
             Total a pagar: {''}
             <span className="font-bold">{formatCurrency(total)}</span>
           </p>
+
+          <form className="w-full mt-20 text-center"
+            action={handleCreateOrder}
+          >
+            <input type="submit"
+              className="py-2 rounded uppercase text-white bg-black w-full text-center cursor-pointer font-bold"
+              value='Confirmar Pedido'
+            ></input>
+          </form>
         </div>
       )}
     </aside>
