68b65545986ce9f0c377d9454b81a6e418568a6f
Ezequiel Lino <sonisombra@gmail.com>
2025-09-18 23:42:55 -0600

    recuperar datos de formulario con formData

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -10,7 +10,9 @@ export default function OrderSummary() {
   const order = useStore((state) => state.order)
   const total = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
 
-  const handleCreateOrder = () => {
+  const handleCreateOrder = (formData: FormData) => {
+    console.log(formData.get('name'));
+    
     createOrder()
   }
 
@@ -19,7 +21,7 @@ export default function OrderSummary() {
       <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
 
       {order.length === 0 ? <p className="text-center my-10">El pedido esta vacio </p> : (
-        <div className="mt-5">
+        <div className="mt-5 p-2">
           {order.map(item => (
             <ProductDetails
               key={item.id}
@@ -32,9 +34,15 @@ export default function OrderSummary() {
             <span className="font-bold">{formatCurrency(total)}</span>
           </p>
 
-          <form className="w-full mt-20 text-center"
+          <form className="w-full mt-10 space-y-5"
             action={handleCreateOrder}
           >
+            <input
+              type="text"
+              placeholder="Tu nombre"
+              className="bg-white border border-gray-100 p-2 w-full"
+              name="name" 
+            />
             <input type="submit"
               className="py-2 rounded uppercase text-white bg-black w-full text-center cursor-pointer font-bold"
               value='Confirmar Pedido'
