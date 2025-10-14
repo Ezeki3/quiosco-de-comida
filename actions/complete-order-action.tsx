0361965c368d993a24a9031514bf74a0c559509c
Ezequiel Lino <sonisombra@gmail.com>
2025-10-13 23:48:27 -0600

    marcar ordenes completas y validacion

--- actions/complete-order-action.tsx
+++ actions/complete-order-action.tsx
@@ -1,6 +1,30 @@
 "use server"
 
-export async function completeOrder(){
-    console.log('desde complete order');
+import { prisma } from "@/src/lib/prisma"
+import { OrderIdSchema } from "@/src/schema"
+
+export async function completeOrder( formData: FormData){
+    const data = {
+        orderId: formData.get('order_id')
+    }
+
+    const result = OrderIdSchema.safeParse(data)
+    
+    if (result.success) {
+        try {
+            await prisma.order.update({
+                where:{
+                    id: result.data.orderId
+                },
+                data: {
+                    status: true,
+                    orderReadyAt: new Date(Date.now())
+                }
+            })
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
     
 }
