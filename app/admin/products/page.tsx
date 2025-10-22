3b6872b8e8dc8dc6e4aac9ab2363edb91d217fa9
Ezequiel Lino <sonisombra@gmail.com>
2025-10-22 14:34:56 -0600

    creando un paginador

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -2,31 +2,42 @@ import ProductTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
 
-async function getProducts(){
+async function getProducts(page: number, pageSize: number) {
+  const skip = (page - 1) * pageSize;
+ 
   const products = await prisma.product.findMany({
+    take: pageSize,
+    skip,
     include: {
-      category: true
-    }
-  })
-
-  return products
+      category: true,
+    },
+  });
+ 
+  return products;
 }
-
-export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>
-
-export default async function ProductsPage() {
-
-  const products = await getProducts()  
-  
+ 
+export type ProductsWithCategory = Awaited<ReturnType<typeof getProducts>>;
+ 
+export default async function ProductsPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  // Espera el objeto searchParams para extraer los parámetros
+  const { page = "1" } = await searchParams;
+ 
+  // Convierte 'page' a número y asigna un valor predeterminado si es inválido
+  const currentPage = parseInt(page as string, 10) || 1;
+  const pageSize = 10;
+ 
+  // Obtén los productos desde la base de datos
+  const products = await getProducts(currentPage, pageSize);
+ 
   return (
     <>
-      <Heading>
-        Administrar Productos
-      </Heading>
-
-      <ProductTable
-        products={products}
-      />
+      <Heading>Manage Products</Heading>
+ 
+      <ProductTable products={products} />
     </>
-  )
+  );
 }
