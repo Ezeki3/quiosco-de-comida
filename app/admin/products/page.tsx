51e82c69d67cc45bd2eb7b70de2fac87cbec361a
Ezequiel Lino <sonisombra@gmail.com>
2025-10-29 00:13:51 -0600

    añadir el boton de pagina anterior

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,3 +1,4 @@
+import { redirect } from "next/navigation";
 import ProductsPagination from "@/components/products/ProductsPagination";
 import ProductTable from "@/components/products/ProductsTable";
 import Heading from "@/components/ui/Heading";
@@ -31,16 +32,25 @@ export default async function ProductsPage({
   // Espera el objeto searchParams para extraer los parámetros
   const { page = "1" } = await searchParams;
  
-  // Convierte 'page' a número y asigna un valor predeterminado si es inválido
-  const currentPage = parseInt(page as string, 10) || 1;
+  // Aseguramos que la página sea al menos 1 para evitar skip negativo
+  const parsedPage = parseInt(page as string, 10);
+  const currentPage = Math.max(1, parsedPage); // Garatiza que no puede ser 0 o negativo
   const pageSize = 10;
+
+  if ( Number(page) < 0) {
+    redirect('/admin/products'); 
+  }
  
   // Obtén los productos desde la base de datos
   const productsData =  getProducts(currentPage, pageSize);
   const totalProductsData =  productCount();
   const [products, totalProducts] = await Promise.all([productsData, totalProductsData])  
   const totalPages = Math.ceil(totalProducts / pageSize)
-  
+
+  if ( Number(page) > totalPages) {
+    redirect('/admin/products'); 
+  }
+
   return (
     <>
       <Heading>Manage Products</Heading>
--- components/products/ProductsPagination.tsx
+++ components/products/ProductsPagination.tsx
@@ -11,6 +11,16 @@ export default function ProductsPagination( {page, totalPages}: ProductsPaginati
 
   return (
     <nav className="flex justify-center py-10">
+
+      {currentPageNumber > 1 && (
+        <Link
+          href={`/admin/products?page=${currentPageNumber - 1}`}
+          className="bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:outline-offset-0"
+        >
+          Anterior &laquo;
+        </Link>
+      )}
+
       { currentPageNumber < totalPages && (
         <Link
           href={`/admin/products?page=${currentPageNumber + 1}`}
