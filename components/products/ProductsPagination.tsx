0b120fcc2426a62ff50fd30b75c842dae9a16152
Ezequiel Lino <sonisombra@gmail.com>
2025-10-29 23:34:01 -0600

    añadir paginador con numeros

--- components/products/ProductsPagination.tsx
+++ components/products/ProductsPagination.tsx
@@ -9,6 +9,8 @@ type ProductsPaginationProps = {
 export default function ProductsPagination( {page, totalPages}: ProductsPaginationProps ) {
   const currentPageNumber = Number(page);
 
+  const pages = Array.from({length: totalPages}, (_, i) => i + 1)  
+
   return (
     <nav className="flex justify-center py-10">
 
@@ -20,6 +22,13 @@ export default function ProductsPagination( {page, totalPages}: ProductsPaginati
           Anterior &laquo;
         </Link>
       )}
+
+      {pages.map(page => (
+        <Link
+          href={`/admin/products?page=${page}`}
+          className={`${currentPageNumber === page && 'font-black'} bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:outline-offset-0`}
+        >{page}</Link>
+      ))}
 
       { currentPageNumber < totalPages && (
         <Link
