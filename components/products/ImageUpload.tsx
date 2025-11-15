2e354637ea7b8453e4c2eee7eafe8e3c9e125707
Ezequiel Lino <sonisombra@gmail.com>
2025-11-14 23:46:01 -0600

    Creando el componente para subir imagenes

--- components/products/ImageUpload.tsx
+++ components/products/ImageUpload.tsx
@@ -1,6 +1,38 @@
+'use client'
+
+import { CldUploadWidget } from "next-cloudinary"
+import { TbPhotoPlus } from "react-icons/tb"
 
 export default function ImageUpload() {
   return (
-    <div>ImageUpload</div>
+    <CldUploadWidget 
+      onSuccess={ (result, {widget}) =>{
+        console.log(result);
+        
+      }}
+      uploadPreset="j5cwjtik" 
+      options={{
+        maxFiles:1
+      }}   
+    >
+      {({open}) => (
+          <>
+            <div className="space-y-2">
+              <label className="text-slate-800">Imagen Producto</label>
+              <div 
+                className="relative cursor-pointer hover:opacity-70 transition p-10 
+                border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600 bg-slate-100"
+                onClick={ () => open()}
+              >
+                <TbPhotoPlus size={50}>
+                  <p className="text-lg font-semibold">Agregar Imagen</p>
+                </TbPhotoPlus>
+              </div>
+            </div>
+          </>
+        )
+
+      }
+    </CldUploadWidget>
   )
 }
