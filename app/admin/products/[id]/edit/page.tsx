1bf507f4e5adcfe65f2e032aec3b04b104546701
Ezequiel Lino <sonisombra@gmail.com>
2025-11-20 23:17:42 -0600

    link para volver atras

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -2,6 +2,7 @@ import EditProductForm from "@/components/products/EditProductsForm";
 import ProductForm from "@/components/products/ProductForm";
 import Heading from "@/components/ui/Heading";
 import { prisma } from "@/src/lib/prisma";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 async function getProductById(id: number){
@@ -25,6 +26,11 @@ export default async function EditProductsPage( {params}: { params: Promise<{id:
   return (
     <>
         <Heading>Editar Producto: {product.name}</Heading>
+
+        <Link
+          href={'/admin/products'}
+          className='bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer'
+        >Volver</Link>
 
         <EditProductForm>
             <ProductForm 
--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -58,6 +58,7 @@ export default async function ProductsPage({
       <Heading>Administrar Productos</Heading>
 
       <div className="flex flex-col lg:flex-row lg:justify-between gap-5">
+        
         <Link
           href={'/admin/products/new'}
           className='bg-amber-400 w-full lg:w-auto text-xl px-10 py-3 text-center font-bold cursor-pointer'
