1d650a6b7497d708ffd3351ef6188927593fa871
Ezequiel Lino <sonisombra@gmail.com>
2025-11-20 22:30:58 -0600

    obtener el producto a editar

---
+++ app/admin/products/[id]/edit/page.tsx
@@ -0,0 +1,26 @@
+import { prisma } from "@/src/lib/prisma";
+import { notFound } from "next/navigation";
+
+async function getProductById(id: number){
+    const product = await prisma.product.findUnique({
+        where: {
+            id
+        }
+    })
+    if (!product) {
+        notFound()
+    }
+
+    return product
+}
+
+export default async function EditProductsPage( {params}: { params: Promise<{id:string}> }) {
+
+    const {id } = await params
+    const product = await getProductById( Number(id) )
+    console.log(product);
+    
+  return (
+    <div>page</div>
+  )
+}
